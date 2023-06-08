bbcdfd0fe1b4f65cb9681d03ff6bb5d8b45ef856
Rian-P <57676700+Rian-P@users.noreply.github.com>
2023-06-08 20:42:20 +0700

    hapus asset

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -26,27 +26,27 @@
                 <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
                     <!-- Item 1 -->
                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="images/background/slide 1.png"
+                        <img src="images/background/slide1.png"
                             class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                     </div>
                     <!-- Item 2 -->
                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="images/background/slide 2.png"
+                        <img src="images/background/slide2.png"
                             class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                     </div>
                     <!-- Item 3 -->
                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="images/background/slide 3.png"
+                        <img src="images/background/slide3.png"
                             class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                     </div>
                     <!-- Item 4 -->
                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="images/background/slide 1.png"
+                        <img src="images/background/slide1.png"
                             class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                     </div>
                     <!-- Item 5 -->
                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="images/background/slide 2.png"
+                        <img src="images/background/slide2.png"
                             class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                     </div>
                 </div>
